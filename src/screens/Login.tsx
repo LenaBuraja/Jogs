e0f6fc5e6af053daaa7ced00ad370e3b12e4f999
Elena Buraya <lenalatkovich@gmail.com>
2020-09-09 17:50:57 +0300

    feat: add immage and change screen login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+scimport { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import { HeaderLine } from '../components/HeaderLine';
+import { RootStackParamList } from '../navigation/RootNavigation';
+import { StackScreenProps } from '@react-navigation/stack';
 
-const Login = () => {
+import BearHead from '../assets/bearFace@2x.png';
+
+type Props = StackScreenProps<RootStackParamList, 'LoginScreen'>;
+
+const Login = ({navigation} : Props) => {
 
 	return (
 		<View style={localeStyles.container}>
 			<HeaderLine />
-			<Text>Login</Text>
+			<Image source={BearHead} style={{height: 100, resizeMode: 'contain'}} />
+			<TouchableOpacity
+				style={localeStyles.button}
+				onPress={() => navigation.navigate('JogsScreen')}
+			>
+				<Text style={localeStyles.text}>Let me in</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -15,7 +27,25 @@ const Login = () => {
 export { Login };
 
 const localeStyles = StyleSheet.create({
+	button: {
+		width: 151,
+		height: 60,
+		borderRadius: 36,
+		borderStyle: 'solid',
+		borderWidth: 3,
+		borderColor: '#e990f9',
+		justifyContent: 'center',
+	},
 	container: {
 		padding: 0,
+	},
+	text: {
+		fontFamily: 'SFUIText',
+		fontSize: 18,
+		fontWeight: 'bold',
+		fontStyle: 'normal',
+		letterSpacing: 0,
+		color: '#e990f9',
+		textAlign: 'center',
 	},
 });
