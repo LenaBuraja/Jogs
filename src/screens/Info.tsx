c4c55bc483a84360b612eb66ff282c052dbc0bc0
Elena Buraya <lenalatkovich@gmail.com>
2020-09-09 21:43:10 +0300

    fix: style screen info

--- src/screens/Info.tsx
+++ src/screens/Info.tsx
@@ -9,24 +9,26 @@ type Props = StackScreenProps<RootStackParamList, 'InfoScreen'>;
 const Info = ({navigation} : Props) => {
 
 	return (
-		<View style={localeStyles.container}>
+		<>
 			<HeaderLine isMenuScreen={false} onPress={() => navigation.navigate('MenuScreen')} />
-			<Text style={localeStyles.title}>Header</Text>
-			<Text>
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-				when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-				It has survived not only five centuries, but also the leap into electronic typesetting, 
-				remaining essentially unchanged. 
-				It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-				and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-			</Text>
-			<Text>
-				It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-				The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
-				content here', making it look like readable English.
-			</Text>
-		</View>
+			<View style={localeStyles.container}>
+				<Text style={localeStyles.title}>INFO</Text>
+				<Text style={localeStyles.text}>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+					Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+					when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+					It has survived not only five centuries, but also the leap into electronic typesetting, 
+					remaining essentially unchanged. 
+					It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+					and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</Text>
+				<Text style={localeStyles.text}>
+					It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+					The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+					content here', making it look like readable English.
+				</Text>
+			</View>
+		</>
 	);
 };
 
@@ -34,15 +36,25 @@ export { Info };
 
 const localeStyles = StyleSheet.create({
 	container: {
-		padding: 0,
+		flex: 1,
+		padding: 25,
+		backgroundColor: '#fff',
 	},
 	title: {
-		fontFamily: 'SFUIText',
+		fontFamily: "SFUIText",
 		fontSize: 25,
-		fontWeight: 'bold',
-		fontStyle: 'normal',
+		fontWeight: "bold",
+		fontStyle: "normal",
 		letterSpacing: 0,
-		textAlign: 'center',
 		color: '#7ed321',
+	},
+	text: {
+		paddingBottom: 50,
+		fontFamily: "SFUIText",
+		fontSize: 12,
+		fontWeight: "normal",
+		fontStyle: "normal",
+		lineHeight: 24,
+		letterSpacing: 0,
 	},
 });
